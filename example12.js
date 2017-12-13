8871c3638cb2c9380c50cacf1b965582c1b902e1
mrostanski <mrostanski@gmail.com>
2017-12-13 15:13:07 +0100

    test

---
+++ example12.js
@@ -0,0 +1,65 @@
+var http = require("http").createServer(handler); // on req - hand
+var io = require("socket.io").listen(http); // socket library
+var fs = require("fs"); // variable for file system for providing html
+var firmata = require("firmata");
+
+console.log("Starting the code");
+
+var board = new firmata.Board("/dev/ttyACM0", function() { // ACM Abstract Control Model for serial communication with Arduino (could be USB)
+    console.log("Arduino connect");
+    board.pinMode(2, board.MODES.OUTPUT); // direction of DC motor
+    board.pinMode(3, board.MODES.PWM); // PWM of motor i.e. speed of rotation
+    board.pinMode(4, board.MODES.OUTPUT); // direction DC motor
+    board.digitalWrite(2,1); // initialization of digital pin 2 to rotate Left on start
+    board.digitalWrite(4,0); // initialization of digital pin 2 to rotate Left on start
+});
+
+function handler(req, res) {
+    fs.readFile(__dirname + "/example12.html",
+    function (err, data) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end("Error loading html page.");
+        }
+    res.writeHead(200);
+    res.end(data);
+    })
+}
+
+http.listen(8080); // server will listen on port 8080
+
+var sendValueViaSocket = function(){};
+
+board.on("ready", function() {
+    io.sockets.on("connection", function(socket) {
+    console.log("Socket id: " + socket.id);
+    socket.emit("messageToClient", "Srv connected, board OK");
+ 
+    socket.on("sendPWM", function(pwm){
+        board.analogWrite(3,pwm);
+        socket.emit("messageToClient", "PWM set to: " + pwm);        
+        console.log("pwm");
+    });
+    
+    socket.on("left", function(value){
+        board.digitalWrite(2,value.AIN1);
+        board.digitalWrite(4,value.AIN2);
+        console.log("left");
+        socket.emit("messageToClient", "Direction: left");
+    });
+    
+    socket.on("right", function(value){
+        board.digitalWrite(2,value.AIN1);
+        board.digitalWrite(4,value.AIN2);
+        socket.emit("messageToClient", "Direction: right");
+    });
+    
+   socket.on("stop", function(value){
+        board.analogWrite(3,value);
+        socket.emit("messageToClient", "STOP");
+    });
+
+    
+    }); // end of sockets.on connection
+
+}); // end of board.on ready
